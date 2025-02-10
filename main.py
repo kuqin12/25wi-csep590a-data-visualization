c08189cb58396addfde73e1841e5d2b8ff925071
Kun Qin <kuqin@microsoft.com>
2025-02-09 23:34:41 -0800

    per player data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from nba_api.stats.endpoints import leaguedashplayerstats
+from nba_api.stats.endpoints import playergamelog
 
 def get_all_players_stats(season):
     player_stats = leaguedashplayerstats.LeagueDashPlayerStats(season=season).get_data_frames()[0]
@@ -13,3 +14,9 @@ def save_stats_to_csv(season, file_name='player_stats.csv'):
 # Example usage
 season = '2022-23'
 print(save_stats_to_csv(season))
+
+player_logs = playergamelog.PlayerGameLog(player_id='203999', season=season)
+player_logs_df = player_logs.get_data_frames()[0]
+
+# Save to CSV
+player_logs_df.to_csv('player_game_logs.csv', index=False)
