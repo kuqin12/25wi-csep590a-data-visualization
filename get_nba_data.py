10edfd3a2cd1cfd5b1bbd30dcbaf409e0e3ef9cd
kuqin12 <42554914+kuqin12@users.noreply.github.com>
2025-02-13 00:16:28 -0800

    Adding data wrangling scripts and output (#1)
    This change adds the initial data wrangling scripts to the repo.

---
+++ get_nba_data.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+from itertools import product
+import urllib.request
+import tarfile
+from typing import Union, Sequence
+
+
+def load_nba_data(path: Union[Path, str] = Path.cwd(),
+                  seasons: Union[Sequence, int] = range(1996, 2023),
+                  data: Union[Sequence, str] = ("datanba", "nbastats", "pbpstats",
+                                                "shotdetail", "cdnnba", "nbastatsv3"),
+                  seasontype: str = 'rg',
+                  league: str = 'nba',
+                  untar: bool = False) -> None:
+    """
+    Loading a nba play-by-play dataset from github repository https://github.com/shufinskiy/nba_data
+
+    Args:
+        path (Union[Path, str]): Path where downloaded file should be saved on the hard disk
+        seasons (Union[Sequence, int]): Sequence or integer of the year of start of season
+        data (Union[Sequence, str]): Sequence or string of data types to load
+        seasontype (str): Part of season: rg - Regular Season, po - Playoffs
+        league (str): Name league: NBA or WNBA
+        untar (bool): Logical: do need to untar loaded archive
+
+    Returns:
+        None
+    """
+    if isinstance(path, str):
+        path = Path(path)
+    if isinstance(seasons, int):
+        seasons = (seasons,)
+    if isinstance(data, str):
+        data = (data,)
+
+    if seasontype == 'rg':
+        need_data = tuple(["_".join([data, str(season)]) for (data, season) in product(data, seasons)])
+    elif seasontype == 'po':
+        need_data = tuple(["_".join([data, seasontype, str(season)]) \
+                           for (data, seasontype, season) in product(data, (seasontype,), seasons)])
+    else:
+        need_data_rg = tuple(["_".join([data, str(season)]) for (data, season) in product(data, seasons)])
+        need_data_po = tuple(["_".join([data, seasontype, str(season)]) \
+                              for (data, seasontype, season) in product(data, ('po',), seasons)])
+        need_data = need_data_rg + need_data_po
+    if league.lower() == 'wnba':
+        need_data = ['wnba_' + x for x in need_data]
+
+    with urllib.request.urlopen("https://raw.githubusercontent.com/shufinskiy/nba_data/main/list_data.txt") as f:
+        v = f.read().decode('utf-8').strip()
+
+    name_v = [string.split("=")[0] for string in v.split("\n")]
+    element_v = [string.split("=")[1] for string in v.split("\n")]
+
+    need_name = [name for name in name_v if name in need_data]
+    need_element = [element for (name, element) in zip(name_v, element_v) if name in need_data]
+
+    for i in range(len(need_name)):
+        t = urllib.request.urlopen(need_element[i])
+        with path.joinpath("".join([need_name[i], ".tar.xz"])).open(mode='wb') as f:
+            f.write(t.read())
+        if untar:
+            with tarfile.open(path.joinpath("".join([need_name[i], ".tar.xz"]))) as f:
+                f.extract("".join([need_name[i], ".csv"]), path)
+
+            path.joinpath("".join([need_name[i], ".tar.xz"])).unlink()
+
+
+if __name__ == "__main__":
+    # if the path does not exist, create it
+    path = Path.cwd().joinpath("nba_data")
+    if not path.exists():
+        path.mkdir()
+
+    load_nba_data(
+        path=path,
+        seasons=range(2014, 2025),
+        untar=True
+    )
---
+++ main.py
@@ -0,0 +1,83 @@
+from nba_api.stats.endpoints import playergamelog
+import pandas as pd
+from nba_api.stats.static import players, teams
+import time
+import random
+from nba_api.stats.endpoints import playerdashptshots, leaguehustlestatsplayer, leaguehustlestatsteam
+import get_nba_data
+
+
+# Run the main function
+if __name__ == "__main__":
+    # Get the list of all players
+    player_list = players.get_players()
+    if not os.path.exists('all_players.csv'):
+        pd.DataFrame(player_list).to_csv('all_players.csv', index=False)
+
+    # Get the list of all teams
+    team_list = teams.get_teams()
+    if not os.path.exists('all_teams.csv'):
+        pd.DataFrame(team_list).to_csv('all_teams.csv', index=False)
+
+    # Data is downloaded from the https://github.com/shufinskiy/nba_data as of 2025-02-11
+    if not os.path.exists('nba_data'):
+        get_nba_data.get_nba_data(path='nba_data', seasons=range(2014, 2025), data=['shotdetail'], untar=True)
+
+    # For all the data in nba, we read all shotdetail_<year> csv into a dataframe
+    full_df = pd.DataFrame()
+    for year in range(2014, 2025):
+        df = pd.read_csv(f"nba_data/shotdetail_{year}.csv")
+        print(f"Dataframe for year {year} has shape {df.shape}")
+        # filter out the non 3pt shots
+        df = df[df['SHOT_TYPE'] == '3PT Field Goal']
+        # Here we aggregate the dataframe to calculate the number of shots made and missed by each player
+        df_p = df.groupby('PLAYER_ID').agg({'SHOT_MADE_FLAG': 'sum', 'SHOT_ATTEMPTED_FLAG': 'sum'}).reset_index()
+        df_p['SEASON'] = year
+        df_p['X_ID'] = df_p['PLAYER_ID']
+        # drop the PLAYER_ID column
+        df_p.drop(columns=['PLAYER_ID'], inplace=True)
+        full_df = pd.concat([full_df, df_p], ignore_index=True)
+
+        # A trick is that here we treat the team the same as the player, so we can aggregate the data
+        df_t = df.groupby('TEAM_ID').agg({'SHOT_MADE_FLAG': 'sum', 'SHOT_ATTEMPTED_FLAG': 'sum'}).reset_index()
+        df_t['SEASON'] = year
+        df_t['X_ID'] = df_t['TEAM_ID']
+        # drop the TEAM_ID column
+        df_t.drop(columns=['TEAM_ID'], inplace=True)
+        full_df = pd.concat([full_df, df_t], ignore_index=True)
+
+    # Save the aggregated dataframe to a CSV file
+    if not os.path.exists('x_shot_summary_2014_2024.csv'):
+        full_df.to_csv('x_shot_summary_2014_2024.csv', index=False)
+
+    # The repo above does not provide the contested data, so we need to get it from the nba_api
+    contested_df = pd.DataFrame()
+    for year in range(2014, 2025):
+        # Get the player hustle stats for the year
+        player_hustle_stats = leaguehustlestatsplayer.LeagueHustleStatsPlayer(
+            season=str(year) + "-" + str(year + 1)[2:]
+        )
+        player_hustle_stats_df = player_hustle_stats.get_data_frames()[0]
+        # Keep the CONTESTED_SHOTS_3PT and PLAYER_NAME columns
+        player_hustle_stats_df = player_hustle_stats_df[['CONTESTED_SHOTS_3PT', 'PLAYER_NAME']]
+        player_hustle_stats_df['SEASON'] = year
+        contested_df = pd.concat([contested_df, player_hustle_stats_df], ignore_index=True)
+        print(f"Dataframe for year {year} has shape {player_hustle_stats_df.shape}")
+
+        # Do the same for the team hustle stats
+        teams_hustle_stats = leaguehustlestatsteam.LeagueHustleStatsTeam(
+            season=str(year) + "-" + str(year + 1)[2:]
+        )
+        team_hustle_stats_df = teams_hustle_stats.get_data_frames()[0]
+        # Keep the CONTESTED_SHOTS_3PT and PLAYER_NAME columns
+        team_hustle_stats_df = team_hustle_stats_df[['CONTESTED_SHOTS_3PT', 'TEAM_NAME']]
+        team_hustle_stats_df['SEASON'] = year
+        contested_df = pd.concat([contested_df, team_hustle_stats_df], ignore_index=True)
+        print(f"Dataframe for year {year} has shape {team_hustle_stats_df.shape}")
+
+        # Sleep for a random time between 1 and 5 seconds
+        time.sleep(random.randint(1, 5))
+
+    # Save the contested shots dataframe to a CSV file
+    if not os.path.exists('contested_shots_2014_2024.csv'):
+        contested_df.to_csv('contested_shots_2014_2024.csv', index=False)    
